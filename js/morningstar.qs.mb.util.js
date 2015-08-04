d6fe85d89d77f54294d3983cab82cf03451dcca9
lyqx123 <alisa.yuan@morningstar.com>
2015-08-04 14:48:36 +0800

    creat a nameSpace and modify Index.js to morningstar.qs.mb.js

---
+++ js/morningstar.qs.mb.util.js
@@ -0,0 +1,8 @@
+﻿function QS_RegisterNameSpaces(nameSpaces) {
+    var parts = nameSpaces.split(".");
+    var root = window;
+    for (var i = 0, len = parts.length; i < len; i++) {
+        root[parts[i]] = new Object();
+        root = root[parts[i]];
+    }
+}
