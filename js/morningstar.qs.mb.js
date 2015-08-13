b4226011f863765a84d74aec1036a30ccfd339d6
bduan <bella.duan@morningstar.com>
2015-08-13 10:42:41 +0800

    make the objectification

--- js/morningstar.qs.mb.js
+++ js/morningstar.qs.mb.js
@@ -7,51 +7,57 @@ QS_RegisterNameSpaces("QS._morningstar._mb");
 //add properties
 QS._morningstar._mb = {
     status: "OneDay",
-    config: {
-        OneDay: {
-            color: [],
-            value: ["&lt;-1.25%", "+1.25%&gt;"],
-            range: [-1.25, 1.25]
-        },
-        OneWeek: {
-            color: [],
-            value: ["&lt;-1.25%", "+1.25%&gt;"],
-            range: [-1.25, 1.25]
-        },
-        OneMouth: {
-            color: [],
-            value: ["&lt;-4%", "+4%&gt;"],
-            range: [-4, 4]
-        },
-        ThreeMouth: {
-            color: [],
-            value: ["&lt;-8%", "+8%&gt;"],
-            range: [-8, 8]
-        },
-        OneYear: {
-            color: [],
-            value: ["&lt;-20%", "+20%&gt;"],
-            range: [-20, 20]
-        },
-        ThreeYear: {
-            color: [],
-            value: ["&lt;-20%", "20%&gt;"],
-            range: [-20, 20]
-        },
+    typedef: ["OneDay", "OneWeek", "OneMonth", "ThreeMonth", "OneYear", "ThreeYear"],
+    fontvalue: ["1D", "1W", "1M", "3M", "1Y", "3Y"],
+    links: ["http://quotes.morningstar.com/indexquote/quote.html?t=$MLVL",
+            "http://quotes.morningstar.com/indexquote/quote.html?t=$MLCR",
+            "http://quotes.morningstar.com/indexquote/quote.html?t=$MLGR",
+            "http://quotes.morningstar.com/indexquote/quote.html?t=$MMVL",
+            "http://quotes.morningstar.com/indexquote/quote.html?t=$MMCR",
+            "http://quotes.morningstar.com/indexquote/quote.html?t=$MMGR",
+            "http://quotes.morningstar.com/indexquote/quote.html?t=$MSVL",
+            "http://quotes.morningstar.com/indexquote/quote.html?t=$MSCR",
+            "http://quotes.morningstar.com/indexquote/quote.html?t=$MSGR"
+    ],
+    colorArr: ["#bf0d3f", "#ea5f80", "#f4afbf", "#999", "#aae3c4", "#56c88a", "#00ac4d"],
+    numValue:["1.25","1.25","4","8","20","20"],
+    attdef: {
+        color:[],
+        value:[],
+        range:[]
     },
+    config:[],
+    init: function (type) {
+        var proToString = JSON.stringify(QS._morningstar._mb.attdef);
+        var properties = "{\"" + type + "\"" + ":" + proToString + "}";
+ 
+        return JSON.parse(properties);
+    },
+  
 
 };
 
 
+
 // through type gain properties
 QS._morningstar._mb.GetConfig = function (type) {
-    return QS._morningstar._mb.config[type];
+    var keyvalue = "";
+    for (var i = 0; i < 6; i++) {
+        for (var key in QS._morningstar._mb.config[i]) {
+            keyvalue = key;
+        }
+        if (keyvalue == type) {
+
+            return QS._morningstar._mb.config[i][type];
+            break;
+        }
+    }
     };
 
 
 
 QS._morningstar._mb.GetColor = function (rangeArr, rate) {
-    var colorArr = ["#bf0d3f", "#ea5f80", "#f4afbf", "#999", "#aae3c4", "#56c88a", "#00ac4d"];
+    var colorArr = QS._morningstar._mb.colorArr;;
     var avg = (rangeArr[1] - rangeArr[0]) / 7;
     var compareNum = rangeArr[0];
     var sub;
@@ -87,6 +93,7 @@ QS._morningstar._mb.ColorReturn = function (periodReturn, config) {
     config.color = [];
     for (var i = 0, len = periodReturn.length; i < len; i++) {
         var color = QS._morningstar._mb.GetColor(config.range, periodReturn[i]);
+      
         config.color.push(color);
     }
     return config.color;
@@ -94,12 +101,10 @@ QS._morningstar._mb.ColorReturn = function (periodReturn, config) {
 
 QS._morningstar._mb.Show = function (periodReturn, type) {
     var config = QS._morningstar._mb.GetConfig(type);
-
     var colorValue = QS._morningstar._mb.ColorReturn(periodReturn, config);
-    //var divArr = $("div[class ='left1'] div[class^='box']");
+    var divArr = $("table[class ='left1'] td[class^='box']");
     for (var i = 0; i < colorValue.length; i++) {
-        document.getElementById("color" + i).style.backgroundColor = colorValue[i];
-        //divArr[i] .css("backgroundColor", colorValue[i]);
+        $(divArr[i]).css("backgroundColor", colorValue[i]);
     }
     for (var j = 0; j < config.value.length; j++) {
         $("#value" + j).innerHTML = config.value[j];
@@ -112,9 +117,13 @@ QS._morningstar._mb.ShowSmall = function (periodReturn, type) {
     if (config) {
         var colorValue = QS._morningstar._mb.ColorReturn(periodReturn, config);
 
-        var divArr = $("div[period='" + type + "'] div[class^='boxs']");
+        var divArr = $("table[period='" + type + "'] td[class^='boxs']");
         for (var i = 0; i < divArr.length; i++) {
             $(divArr[i]).css("backgroundColor", colorValue[i]);
+        }
+        var tableArr = $("table[period='" + type + "'] td[class='boxs']");
+        for (var i = 0; i < tableArr.length; i++) {
+            $(tableArr[i]).css("backgroundColor", colorValue[i]);
         }
     }
 }
@@ -139,20 +148,24 @@ QS._morningstar._mb.Update = function () {
 
 
 QS._morningstar._mb.Subscribe = function () {
+  
     var me = this;
-    var boxs= $("div[class='boxsmall']");
+    var boxs= $("table[class='boxsmall']");
     var type = "";
     var callbackSub = function (dataBase) {
         QS._morningstar._mb.Show(dataBase.root[QS._morningstar._mb.status].split(','), QS._morningstar._mb.status);
         for (var i = 0, len = boxs.length; i < len; i++) {
             var box = boxs[i];
             type = box.attributes["period"].value;
+           
+         
             var periodReturn = dataBase.root[type].split(',');
             QS._morningstar._mb.ShowSmall(periodReturn, type);
             if (box.attributes["period"]) {
                 box.addEventListener("mouseover", function (periodReturn,type) {
                     return function () {
-                        QS._morningstar._mb.status = type;
+                        //QS._morningstar._mb.status = type;
+                        //console.log(type);
                         QS._morningstar._mb.Show(periodReturn, type);
                     }
                 }(periodReturn,type))
@@ -164,10 +177,65 @@ QS._morningstar._mb.Subscribe = function () {
 
 
 
+//jquery动态生成一个九宫格
+QS._morningstar._mb.createSmallOne = function (len,type,attr1,attr2) {
+    var txt = "<table class="+attr1+" period="+type+">";
+    for (var i = 0; i < len; i++) {
+        txt = txt + "<tr>";
+        for (var j = 0; j < len; j++) {
+            txt = txt+"<td class="+attr2+"></td>";
+        }
+        txt = txt + "</tr>";
+    }
+    txt = txt + "</table>"
+    return txt
+}
+
+
+//创建一个2*3的表格，用来存放小九宫格
+QS._morningstar._mb.createBig = function (rows, cols) {
+    var txt = "<table class='smallone'>";
+    for (var i = 0; i < rows; i++) {
+        txt = txt + "<tr class='tr1' >";
+        for (var j = 0; j < cols; j++) {
+            var type = QS._morningstar._mb.typedef[cols * i + j];
+            var jsObj = QS._morningstar._mb.init(type);
+             QS._morningstar._mb.config.push(jsObj);
+            var rang = QS._morningstar._mb.numValue[cols * i + j];
+            var rangt = "{\"-" + rang + "\",\"" + rang + "\"}";
+            var valuet = "&lt;-"+rang+"%\",\"&gt;+"+rang+"%";
+            jsObj[type].range.push(-rang);
+            jsObj[type].range.push(rang);
+            //console.log(range);
+            jsObj[type].value.push(valuet);
+            //console.log(valuet);
+        txt = txt + "<td class='td1'>" + QS._morningstar._mb.createSmallOne(3,type , "boxsmall","boxs") + "<span>" + QS._morningstar._mb.fontvalue[3 * i + j] + "</span></td>";
+    }
+    }
+   
+    $(".Small").html(txt);
+}
+
+//创建一个大的九宫格
+QS._morningstar._mb.createBigOne = function () {
+    var txt = QS._morningstar._mb.createSmallOne(3, QS._morningstar._mb.status, "left1", "box");
+    $(".Big").html(txt);
+    var tdlen = $("table[class='left1'] td[class='box']");
+    var txtt = "";
+    for (var i = 0, len = tdlen.length; i < len; i++) {
+        txtt = "<a href=" + QS._morningstar._mb.links[i] + "></a>"
+       $(txtt).appendTo(tdlen[i]);
+     
+    }
+}
+
 window.onload = function () {
+    QS._morningstar._mb.createBigOne();
+    QS._morningstar._mb.createBig(2, 3);
     QS._morningstar._mb.Subscribe();
-    //MorningStraMarketBarometer.Update();
-    setInterval(QS._morningstar._mb.Update, 5000);
+
+    QS._morningstar._mb.Update();
+    //setInterval(QS._morningstar._mb.Update, 1000);
 }
 
 
